5c44581927c63af6d99100c0e710d509584f3943
sampada <sampadabjkmb@gmail.com>
2024-06-09 22:12:49 +0530

    first commitss

--- dfs.py
+++ dfs.py
@@ -1,2 +1,21 @@
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+visited = set() 
+def dfs(visited, graph, node):   
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+print("Enter the starting node for Depth-First Search:")
+start_node = input().strip()
+print("Following is the Depth-First Search:")
+dfs(visited, graph, start_node)
 
             
